2d2d92c45906252ce24f0ba9a1d1bf29a193cc8f
mskwon25 <canopus.kwon@gmail.com>
2017-05-18 12:57:59 +0900

    add 5557.c

--- BOJ/DP/5557.c
+++ BOJ/DP/5557.c
@@ -1,16 +1,24 @@
 #include<stdio.h>
+#include<string.h>
 
-int d[101][20000];
 int a[101];
-int go(int i,int j){
-  if(i==0) return 0;
-  if(d[i][j] >=0) return d[i][j];
+long long d[101][21];
+int n;
 
-  d[i][j] = go(i-1, j-a[i]) + go(i-1, j+a[i]);
+long long go(int i, int j){
+  if(i<0 || j<0 || j>20) return 0;
+  if(i==1 && j==a[1]) return 1;
+  if(d[i][j] != -1) return d[i][j];
   
+  d[i][j] = go(i-1,j+a[i]) + go(i-1,j-a[i]);
+  return d[i][j];
 }
-
 int main(){
   scanf("%d", &n);
   for(int i=1; i<=n; i++)
     scanf("%d", &a[i]);
+  
+  memset(d,-1,sizeof(d));
+  printf("%lld\n", go(n-1,a[n]));
+  return 0;
+}
